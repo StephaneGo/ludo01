788488b39fb854e8f085d75502cc09e57907a353
Stephane <sgobin@campus-eni.fr>
2025-03-20 14:15:12 +0100

    s005-ajout Genre

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,13 @@
 truncate table clients;
 truncate table adresses;
+truncate table genres;
 
+insert into genres (no_genre, libelle) values (1, 'Jeu de plateau');
+insert into genres (no_genre, libelle) values (2, 'Jeu de cartes');
+insert into genres (no_genre, libelle) values (3, 'Jeu de stratégie');
+insert into genres (no_genre, libelle) values (4, 'Coopératif');
+insert into genres (no_genre, libelle) values (5, 'Jeu de dé');
+insert into genres (no_genre, libelle) values (6, 'Jeu d''enquete');
 insert into adresses(rue, code_postal, ville) values ('rue des Cormorans', '79000', 'Niort');
 INSERT INTO CLIENTS (nom, prenom, email, no_telephone, no_adresse) VALUES ('Curie', 'Marie', 'marie.curie@example.com', '123456789', scope_identity());
 insert into adresses(rue, code_postal, ville) values ('rue des marguerites', '79500', 'Melle');
